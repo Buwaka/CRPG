5a5c1ca29634006f601daf367209b7d2b7f955b7
BakaProxy <sammywyd@gmail.com>
2021-05-07 22:35:50 +0200

    init

---
+++ Assets/Scripts/Field/Field Node.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FieldNode : MonoBehaviour
+{
+    public List<FieldNode> Connections = new List<FieldNode>();
+
+
+    //gizmo shit
+    public float Gizmo_Size = 1.0f;
+    public bool Gizmo_Selected = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Connect(FieldNode node)
+    {
+        if (IsConnected(node))
+            return;
+
+        Connections.Add(node);
+    }
+
+    public void Disconnect(FieldNode node)
+    {
+        if (!IsConnected(node))
+            return;
+
+        Connections.Remove(node);
+    }
+
+    public void FlipConnection(FieldNode node)
+    {
+        bool thisConnection = Connections.Contains(node);
+        bool thatConnection = node.IsConnected(this);
+
+        if (!thisConnection && !thatConnection)
+            return;
+
+        if (thisConnection && thatConnection)
+        {
+            node.Disconnect(this);
+        }
+        else if (thisConnection)
+        {
+            Disconnect(node);
+            node.Connect(this);
+        }
+        else if (thatConnection)
+        {
+            Connect(node);
+            node.Disconnect(this);
+        }
+
+    }
+
+    public bool IsConnected(FieldNode node)
+    {
+        return Connections.Contains(node);
+    }
+
+
+
+
+    public void DrawArrow(Vector3 from, Vector3 to, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 10.0f)
+    {
+        Gizmos.color = color;
+        Vector3 direction = to - from;
+        Gizmos.DrawRay(from, direction);
+
+        Vector3 right = Quaternion.Euler(0, 0, arrowHeadAngle) * -direction;
+        Vector3 left = Quaternion.Euler(0, 0, -arrowHeadAngle) * -direction;
+        Gizmos.DrawRay(to, right * arrowHeadLength);
+        Gizmos.DrawRay(to, left * arrowHeadLength);
+    }
+
+    void OnDrawGizmos()
+    {
+
+
+        for (int i = 0; i < Connections.Count; i++)
+        {
+            var connection = Connections[i];
+            var color = Color.Lerp(Color.blue, Color.red, (float)i / Connections.Count);
+            DrawArrow(transform.position, connection.transform.position, color);
+        }
+
+        if (Gizmo_Selected)
+            Gizmos.color = Color.yellow;
+        else
+            Gizmos.color = Color.white;
+        Gizmos.DrawSphere(transform.position, Gizmo_Size);
+    }
+}
---
+++ Assets/Scripts/Map Editor/MapCreator.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class MapCreator : EditorWindow
+{
+    private Transform parent = null;
+
+    private bool paintMode = false;
+    private bool ParentSelect = false;
+
+
+    private List<FieldNode> Selected = new List<FieldNode>();
+
+    // Start is called before the first frame update
+    [MenuItem("CRPG/Node Map Editor")]
+    private static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MapCreator));
+    }
+
+    // Called to draw the MapEditor windows.
+    private void OnGUI()
+    {
+        if (ParentSelect)
+        {
+            if (Event.current.type == EventType.Layout && Event.current.button == 0)
+            {
+                var objs = Selection.GetTransforms(SelectionMode.Editable);
+
+                if (objs.Length > 0)
+                {
+                    parent = objs[0];
+                    ParentSelect = false;
+                }
+            }
+        }
+
+        paintMode = GUILayout.Toggle(paintMode && parent, "Start painting", "Button", GUILayout.Height(60f));
+
+
+        GUILayout.Label("Parent");
+        EditorGUILayout.BeginHorizontal();
+        ParentSelect = GUILayout.Toggle(ParentSelect, "Select Parent", "Button");
+
+        if (GUILayout.Button("Reset Parent"))
+            parent = null;
+
+        string pname = parent ? parent.name : "No Parent Selected";
+        GUILayout.TextArea(pname);
+        EditorGUILayout.EndHorizontal();
+
+        if (parent)
+        {
+            GUILayout.Label("Actions");
+            EditorGUILayout.BeginHorizontal();
+
+
+            EditorGUI.BeginDisabledGroup(Selected.Count < 2);
+            if (GUILayout.Button("Connect"))
+                ConnectSelectedAll();
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUI.BeginDisabledGroup(Selected.Count != 2);
+            if (GUILayout.Button("Connect One Way"))
+                ConnectSelectedOne();
+            if (GUILayout.Button("Flip Connection"))
+                FlipSelected();
+            EditorGUI.EndDisabledGroup();
+
+
+
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+    }
+
+    // Does the rendering of the map editor in the scene view.
+    private void OnSceneGUI(SceneView sceneView)
+    {
+        Ray guiRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        Vector3 mousePosition = guiRay.origin - guiRay.direction * (guiRay.origin.z / guiRay.direction.z);
+
+
+
+        if (paintMode && parent)
+        {
+            PaintMode(mousePosition);
+        }
+        else
+        {
+            ResetSelection();
+        }
+    }
+
+
+    void PaintMode(Vector3 Position)
+    {
+        //right mouse button - create & reset
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 1)
+        {
+            if (Selected.Count > 0)
+            {
+                ResetSelection(); 
+
+            }
+            else
+            {
+                CreateNode(Position);
+            }
+
+        }
+
+        //left mouse key - select
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+            //ConnectNode(mousePosition);
+            FieldNode node = FindNode(Position);
+            if (node)
+            {
+                if (!Event.current.control)
+                {
+                    ResetSelection();
+                }
+
+                SelectNode(node);
+            }
+            else
+            {
+                ResetSelection();
+            }
+        }
+    }
+
+    void CreateNode(Vector3 Position)
+    {
+        // Create the prefab instance while keeping the prefab link
+        GameObject obj = new GameObject();
+        obj.AddComponent<FieldNode>();
+        obj.transform.position = Position;
+        obj.transform.parent = parent;
+
+        // Allow the use of Undo (Ctrl+Z, Ctrl+Y).
+        Undo.RegisterCreatedObjectUndo(obj, "");
+    }
+
+    FieldNode FindNode(Vector3 Position)
+    {
+        var children = parent.GetComponentsInChildren<FieldNode>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            var child = children[i];
+            if (Mathf.Abs((Position - child.transform.position).magnitude) < child.Gizmo_Size)
+            {
+                return child;
+            }
+        }
+
+        return null;
+    }
+
+    void SelectNode(FieldNode select)
+    {
+        select.Gizmo_Selected = true;
+        Selected.Add(select);
+        Repaint();
+    }
+
+    void DeselectNode(FieldNode select)
+    {
+        select.Gizmo_Selected = false;
+        Selected.Remove(select);
+        Repaint();
+    }
+
+    void ResetSelection()
+    {
+        foreach (var select in Selected)
+        {
+            select.Gizmo_Selected = false;
+        }
+        Selected.Clear();
+        Repaint();
+    }
+
+    void ConnectSelectedAll()
+    {
+        if (Selected.Count < 2)
+            return;
+
+        for (int i = 0; i < Selected.Count; i++)
+        {
+            var select = Selected[i];
+            for (int j = 0; j < Selected.Count; j++)
+            {
+                var subselect = Selected[j];
+                if (select != subselect)
+                {
+                    select.Connect(subselect);
+                }
+            }
+        }
+    }
+
+    void ConnectSelectedOne()
+    {
+        if (Selected.Count != 2)
+            return;
+
+        Selected[0].Connect(Selected[1]);
+    }
+
+    void FlipSelected()
+    {
+        if (Selected.Count != 2)
+            return;
+
+        Selected[0].FlipConnection(Selected[1]);
+    }
+
+    void OnFocus()
+    {
+        SceneView.duringSceneGui -= this.OnSceneGUI; // Just in case
+        SceneView.duringSceneGui += this.OnSceneGUI;
+    }
+
+    void OnDestroy()
+    {
+        SceneView.duringSceneGui -= this.OnSceneGUI;
+    }
+}
